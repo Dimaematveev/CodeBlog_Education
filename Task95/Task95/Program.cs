9d6ac859630b7cbec5aa671930baabcbc4a7027f
dimaematveev <dimaematveev@gmail.com>
2019-07-06 20:35:04 +0300

    Сделано).

--- Task95/Task95/Program.cs
+++ Task95/Task95/Program.cs
@@ -10,6 +10,19 @@ namespace Task95
     {
         static void Main(string[] args)
         {
+            //#Task 95
+            //Напечатать все N-значные натуральные числа,
+            //в десятичной записи которых нет одинаковых цифр.
+            const int N = 6;
+
+            int begin = (int)Math.Pow(10,N-1) ;
+            int end = (int)Math.Pow(10, N) -1;
+            for (int i = begin; i < end; i++)
+            {
+               if(i.ToString().Distinct().Count()==N)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
