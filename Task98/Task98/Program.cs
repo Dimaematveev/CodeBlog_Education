5ca804b81487db9c84b5967a7c39d630698009fd
dimaematveev <dimaematveev@gmail.com>
2019-07-06 16:31:10 +0300

    Готово задание.

--- Task98/Task98/Program.cs
+++ Task98/Task98/Program.cs
@@ -10,6 +10,61 @@ namespace Task98
     {
         static void Main(string[] args)
         {
+
+            //      # Task 98. 
+            //  Астрономы часто изображают карту звездного неба на бумаге, где каждая звезда имеет декартовы координаты. 
+            //  Пусть уровнем звезды будет количество других звезд, которые расположены на карте не выше и не правее данной звезды. 
+            //  Астрономы решили узнать уровни всех звезд. 
+            //  Требуется определить, сколько звезд каждого уровня имеется на карте.
+
+            //  |
+            //  |
+            //  |
+            //  |  *     *
+            //  |
+            //  |    *   *
+            //  |_______________
+
+
+
+            List<Coordinate> coordinates = new List<Coordinate>();
+            Random random = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < 1000; i++)
+            {
+                coordinates.Add(new Coordinate(random.Next(-100, 100), random.Next(-100, 100)));
+            }
+            var levels = new List<List<Coordinate>>();
+            foreach (var item in coordinates)
+            {
+                var level = coordinates.Where(coordinate => (coordinate.X <= item.X) && (coordinate.Y <= item.Y)).ToList();
+                levels.Add(level);
+            }
+
+            var groupByLevels = levels.GroupBy(x => x.Count)
+                                        .OrderBy(x => x.Key);
+            foreach (var item in groupByLevels)
+            {
+                Console.WriteLine($"Уровень = {item.Key}, Количество звезд = {item.Count()}");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+    public class Coordinate
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Z = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{X,5};{Y,5};{Z,5}]";
         }
     }
 }
